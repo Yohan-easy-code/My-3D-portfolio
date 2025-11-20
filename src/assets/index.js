c0482a1d9bc827ee7aeeb45831490539aab5742f
Yohan-easy-code <yoyozouari@gmail.com>
2025-11-20 18:42:57 +0100

    add work section

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,12 +25,20 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import dsp from "./company/dsp.png";
+import bhcompetences from "./company/bh-competences.png";
+import agence39 from "./company/agence39.png";
+import freelance from "./company/freelance.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  freelance,
+  agence39,
+  bhcompetences,
+  dsp,
   logo,
   backend,
   creator,
